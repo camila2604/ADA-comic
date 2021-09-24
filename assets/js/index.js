cf0e71aead9f2ed910a0bc8db5330a8ac0255c4f
ayelencristi <ayelendcristi@gmail.com>
2021-09-23 22:39:40 -0300

    correcición de función de filtros

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,7 @@
 var base_url = 'https://gateway.marvel.com';
 var api_key = '84d0b9f6b697a08a98c0419cce4cfb38';
 var HASH = '4a467ffce992bde84ba21eba59dd7d4a';
-fetch(base_url + "/v1/public/comics?apikey=" + api_key + "&hash=" + HASH)
-    .then(function (response) {
-    return response.json();
-}).then(function (_a) {
-    var data = _a.data;
-    console.log(data);
-    var comics = data.results;
+var generateCards = function (comics) {
     comics.forEach(function (comic) {
         var divComics = document.getElementById('container-comics');
         var divCard = document.createElement('div');
@@ -27,4 +21,43 @@ fetch(base_url + "/v1/public/comics?apikey=" + api_key + "&hash=" + HASH)
         divCard.appendChild(imgComic);
         divComics.appendChild(divCard);
     });
+};
+fetch(base_url + "/v1/public/comics?apikey=" + api_key + "&hash=" + HASH)
+    .then(function (response) {
+    return response.json();
+}).then(function (_a) {
+    var data = _a.data;
+    // console.log(data);
+    var totalResult = data.total;
+    var results = document.getElementById('results');
+    results.innerText = totalResult + ' RESULTADOS';
+    var comics = data.results;
+    generateCards(comics);
 });
+var selectType = document.getElementById('select_type');
+var search_select = function () {
+    var type = selectType.value;
+    console.log(type);
+    if (type === 'comics') {
+        fetch(base_url + "/v1/public/comics?&apikey=" + api_key + "&hash=" + HASH)
+            .then(function (response) {
+            return response.json();
+        })
+            .then(function (rta) {
+            console.log(rta);
+            var comics = rta.data.results;
+            generateCards(comics);
+        });
+    }
+    else if (type === 'personajes') {
+        fetch(base_url + "/v1/public/characters?&apikey=" + api_key + "&hash=" + HASH)
+            .then(function (response) {
+            return response.json();
+        })
+            .then(function (rta) {
+            var comics = rta.data.results;
+            generateCards(comics);
+        });
+    }
+};
+selectType.addEventListener('change', search_select);
--- assets/ts/index.ts
+++ assets/ts/index.ts
@@ -2,38 +2,74 @@ const base_url: string = 'https://gateway.marvel.com';
 const api_key: string = '84d0b9f6b697a08a98c0419cce4cfb38';
 const HASH: string = '4a467ffce992bde84ba21eba59dd7d4a';
 
+const generateCards = (comics) => {
+    comics.forEach(comic => {
+        const divComics = document.getElementById('container-comics');
+        const divCard = document.createElement('div');
+        divCard.classList.add('card');
+        divCard.classList.add('col-10');
+        divCard.classList.add('m-3')
+        divCard.style.width = '20rem';
+
+        const divTitle = document.createElement('div');
+        divTitle.classList.add('card-body');
+        const title = document.createElement('h5');
+        title.innerText = comic.title;
+
+        const imgComic = document.createElement('img');
+        imgComic.classList.add('car-img-top');
+        imgComic.src = comic.thumbnail.path + '.' + comic.thumbnail.extension;
+        
+        divCard.appendChild(divTitle);
+        divTitle.appendChild(title);
+        divCard.appendChild(imgComic);
+        divComics.appendChild(divCard);
+    
+    });
+}
+
 fetch(`${base_url}/v1/public/comics?apikey=${api_key}&hash=${HASH}`)
     .then((response) =>{
         return response.json();
     }).then(({data})=>{
-        console.log(data);
+        // console.log(data);
+        const totalResult = data.total;
+        const results = document.getElementById('results');
+        results.innerText = totalResult + ' RESULTADOS';
+
         const comics: Comic[] = data.results;
 
-        
-            comics.forEach(comic => {
-                const divComics = document.getElementById('container-comics');
-                const divCard = document.createElement('div');
-                divCard.classList.add('card');
-                divCard.classList.add('col-10');
-                divCard.classList.add('m-3')
-                divCard.style.width = '20rem';
-
-                const divTitle = document.createElement('div');
-                divTitle.classList.add('card-body');
-                const title = document.createElement('h5');
-                title.innerText = comic.title;
-
-                const imgComic = document.createElement('img');
-                imgComic.classList.add('car-img-top');
-                imgComic.src = comic.thumbnail.path + '.' + comic.thumbnail.extension;
-                
-                divCard.appendChild(divTitle);
-                divTitle.appendChild(title);
-                divCard.appendChild(imgComic);
-                divComics.appendChild(divCard);
+        generateCards(comics);
             
-            });
-    
-
-
     })
+
+    const selectType= <HTMLInputElement>document.getElementById('select_type');
+
+
+
+const search_select = () => {
+    const type = selectType.value;
+    console.log(type);
+    if (type === 'comics') {
+        fetch(`${base_url}/v1/public/comics?&apikey=${api_key}&hash=${HASH}`)
+        .then((response) => {
+            return response.json()
+        })
+        .then(rta => {
+            console.log(rta);
+            const comics = rta.data.results
+            generateCards(comics);
+        });
+    } else if (type === 'personajes') {
+        fetch(`${base_url}/v1/public/characters?&apikey=${api_key}&hash=${HASH}`)
+    .then((response) => {
+    return response.json()
+    })
+    .then(rta => {
+        const comics = rta.data.results
+        generateCards(comics);
+    })
+    }
+}
+
+selectType.addEventListener('change', search_select)
