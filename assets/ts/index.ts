14b16dc309c977f9bf491a8d237d0875eda0404a
ayelencristi <ayelendcristi@gmail.com>
2021-09-25 13:44:46 -0300

    funcion init y filtros

--- assets/ts/index.ts
+++ assets/ts/index.ts
@@ -1,4 +1,4 @@
-const base_url: string = 'https://gateway.marvel.com';
+const base_url: string = 'https://gateway.marvel.com/v1/public/';
 const api_key: string = '84d0b9f6b697a08a98c0419cce4cfb38';
 const HASH: string = '4a467ffce992bde84ba21eba59dd7d4a';
 
@@ -31,48 +31,120 @@ const generateCards = (comics) => {
     });
 }
 
-fetch(`${base_url}/v1/public/comics?apikey=${api_key}&hash=${HASH}`)
-    .then((response) =>{
-        return response.json();
-    }).then(({data})=>{
-        // console.log(data);
-        const totalResult = data.total;
-        const results = document.getElementById('results');
-        results.innerText = totalResult + ' RESULTADOS';
+// &${searchKey}=${s}
+const fetchData = (type, s, order) => {
 
-        const comics: Comic[] = data.results;
+    let url = `${base_url}${type}?apikey=${api_key}&hash=${HASH}&orderBy=${order}`;
 
-        generateCards(comics);
-            
-    })
-
-    const selectType= <HTMLInputElement>document.getElementById('select_type');
-
-
-
-const search_select = () => {
-    const type = selectType.value;
-    console.log(type);
-    if (type === 'comics') {
-        fetch(`${base_url}/v1/public/comics?&apikey=${api_key}&hash=${HASH}`)
-        .then((response) => {
-            return response.json()
-        })
-        .then(rta => {
-            console.log(rta);
-            const comics = rta.data.results
-            generateCards(comics);
-        });
-    } else if (type === 'personajes') {
-        fetch(`${base_url}/v1/public/characters?&apikey=${api_key}&hash=${HASH}`)
-    .then((response) => {
-    return response.json()
-    })
-    .then(rta => {
-        const comics = rta.data.results
-        generateCards(comics);
-    })
+    if(s) {
+        if(type === 'comics') {
+            url += `&title=${s}`;
+        } else {
+            url += `&name=${s}`;
+        }
     }
+
+    fetch(url)
+        .then((response) =>{
+            return response.json();
+        }).then(({data})=>{
+            // console.log(data);
+            const totalResult = data.total;
+            const results = document.getElementById('results');
+            results.innerText = totalResult + ' RESULTADOS';
+
+            const comics: Comic[] = data.results;
+
+            generateCards(comics);
+                
+        })
 }
 
-selectType.addEventListener('change', search_select)
+const init = () => {
+    const params = new URLSearchParams(window.location.search);
+    const search = params.get('s');
+    const type = params.get('type');
+    const order = params.get('order');
+    fetchData(type, search, order);
+    }
+init();
+
+
+
+
+const searchForm = document.getElementById('form-search');
+const btnSearch = document.getElementById('btn-search');
+let url= '';
+
+const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    const search = e.target.title.value;
+    const type = e.target.type.value;
+    const order = e.target.order.value;
+   
+    const params = new URLSearchParams(window.location.search);
+    params.set('s', search);
+    params.set('type', type);
+    params.set('order', order);
+    window.location.href=window.location.hash+'?'+params.toString()
+};
+
+const handlePaginationClick = () => {
+
+}
+
+searchForm.addEventListener('submit', handleSearchSubmit);
+
+// const search_select = () => {
+//     fetch(url)
+//     .then((response) => {
+//         return response.json()
+//     })
+//     .then(rta => {
+//         const comics = rta.data.results
+//         generateCards(comics);
+//     });
+// }
+
+// const search_select = () => {
+//     fetch(`${base_url}${type}&apikey=${api_key}&hash=${HASH}&${search}&${order}`);
+//     .then((response) => {
+//         return response.json()
+//     })
+//     .then(rta => {
+//         console.log(rta);
+//         const comics = rta.data.results
+//         generateCards(comics);
+//     });
+// } 
+
+// selectType.addEventListener('change', search_select)
+
+
+
+
+
+// const search_select = () => {
+//     if (type === 'comics') {
+//         fetch(`${base_url}/v1/public/comics?&apikey=${api_key}&hash=${HASH}`)
+//         .then((response) => {
+//             return response.json()
+//         })
+//         .then(rta => {
+//             console.log(rta);
+//             const comics = rta.data.results
+//             generateCards(comics);
+//         });
+//     } else if (type === 'personajes') {
+//         fetch(`${base_url}/v1/public/characters?&apikey=${api_key}&hash=${HASH}`)
+//     .then((response) => {
+//     return response.json()
+//     })
+//     .then(rta => {
+//         const comics = rta.data.results
+//         generateCards(comics);
+//     })
+//     }
+// }
+
+// selectType.addEventListener('change', search_select)
